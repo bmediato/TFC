97a4cd80f8a1d1c453c605d365cd4b092e060a0b
Beatriz <beatriz.mediatto2@gmail.com>
2023-04-04 15:55:16 -0300

    .

--- app/backend/src/interfaces/IMatches.ts
+++ app/backend/src/interfaces/IMatches.ts
@@ -7,3 +7,10 @@ export interface INewMatches extends IMatches {
   homeTeamId: number;
   awayTeamId: number;
 }
+
+export interface AllMatches extends INewMatches {
+  id: number;
+  inProgress: boolean;
+  homeTeam: { teamName: string };
+  awayTeam: { teamName: string };
+}
---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,24 @@
+// import * as sinon from 'sinon';
+// import * as chai from 'chai';
+// // @ts-ignore
+// import chaiHttp = require('chai-http');
+
+// import { app } from '../app';
+// import { Response } from 'superagent';
+// import Matches from '../database/models/Matches';
+// import all from './mock/matches.mock';
+// import { AllMatches } from '../interfaces/IMatches';
+
+// chai.use(chaiHttp);
+
+// const { expect } = chai;
+
+// describe('rota /matches', () => {
+//   describe('Usando o método GET', () => {
+//     let chaiHttpResponse: Response;
+
+//     before(async () => {
+//       sinon.stub(Matches, 'findAll').resolves(all as AllMatches[])
+//     })
+//   });
+// });
